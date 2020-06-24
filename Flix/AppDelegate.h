216404c7a74f536312e6de2261196ad8a74d3a60
Fernando Arturo Perez <fernandoa@fb.com>
2020-06-24 09:35:21 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Fernando Arturo Perez on 6/24/20.
+//  Copyright © 2020 Fernando Arturo Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
