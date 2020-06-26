72cf27836db8e142980e21c86c1ffbe355aef0c4
Fernando Arturo Perez <fernandoa@fb.com>
2020-06-26 16:46:16 -0700

    Images fade animations implemented.

--- Flix/View Controllers/MoviesGridViewController.m
+++ Flix/View Controllers/MoviesGridViewController.m
@@ -141,7 +141,30 @@
     
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLStr];
     cell.posterView.image = nil;
-    [cell.posterView setImageWithURL:posterURL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:posterURL];
+    __weak MovieCollectionCell *weakSelf = cell;
+    [cell.posterView setImageWithURLRequest:request placeholderImage:nil
+    success:^(NSURLRequest *imageRequest, NSHTTPURLResponse *imageResponse, UIImage *image) {
+        
+        // imageResponse will be nil if the image is cached
+        if (imageResponse) {
+            NSLog(@"Image was NOT cached, fade in image");
+            weakSelf.posterView.alpha = 0.0;
+            weakSelf.posterView.image = image;
+            
+            //Animate UIImageView back to alpha 1 over 0.3sec
+            [UIView animateWithDuration:0.3 animations:^{
+                weakSelf.posterView.alpha = 1.0;
+            }];
+        }
+        else {
+            NSLog(@"Image was cached so just update the image");
+            weakSelf.posterView.image = image;
+        }
+    }
+    failure:^(NSURLRequest *request, NSHTTPURLResponse * response, NSError *error) {
+        NSLog(@"Failed request");
+    }];
     
     
     return cell;
